6d5208abde8cb77e444f1963cd340b0ea4da4a89
Hannah Carney <hannahcarney@Hannahs-MacBook-Pro.local>
2018-11-13 22:58:36 +0000

    still broken

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = process.env.target || 'development';
-process.env.NODE_ENV = process.env.target || 'development';
+process.env.BABEL_ENV = 'production';
+process.env.NODE_ENV = 'production';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -20,7 +20,7 @@ const chalk = require('chalk');
 const fs = require('fs-extra');
 const webpack = require('webpack');
 const bfj = require('bfj');
-const config = require(`../config/webpack.config.${process.env.NODE_ENV}`);
+const config = require('../config/webpack.config.production');
 const paths = require('../config/paths');
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
@@ -131,7 +131,7 @@ function async(context) {
 
 // Create the production build and print the deployment instructions.
 function build(previousFileSizes, context) {
-  console.log(`Creating a ${process.env.NODE_ENV} build...`);
+  console.log('Creating an optimized production build...');
 
   let compiler = webpack(config);
   return new Promise((resolve, reject) => {
@@ -182,14 +182,13 @@ function build(previousFileSizes, context) {
         return bfj
           .write(paths.appBuild + '/bundle-stats.json', stats.toJson())
           .then(() => {
-            // var prepare = require('./Prepare.js')
-            // prepare.Prepare(context);
-            // return resolve(resolveArgs)
-            return resolve(resolveArgs);
+            var Prepare = require('./Prepare.js')
+            new Prepare(context);
+            return resolve(resolveArgs)
           })
           .catch(error => reject(new Error(error)));
       }
-   
+      return resolve(resolveArgs);
     });
   });
 }
