2e009a813f9b5f7510a60316fa9badbba0f30674
Hannah Carney <hannahcarney@Hannahs-MacBook-Pro.local>
2018-11-14 02:04:32 +0000

    can reload multiple times now on ios and browser

--- scripts/Prepare.js
+++ scripts/Prepare.js
@@ -112,15 +112,6 @@ function Prepare(context) {
         }
 
         patcher.setConfig(servers.external) 
-
-        // wee hack to override the cordova native browser run script
-        var theSourceFile = path.join(path.resolve()) + '/scripts/start.js';
-        fs.readFile(theSourceFile, function (err, buf) {
-            if (typeof buf !== 'undefined') {
-                var theDestinationFile = path.join(path.resolve()) + '/platforms/browser/lib/run.js';
-                fs.writeFile(theDestinationFile, buf.toString(), function (err) { });
-            };
-        });
         return deferral.resolve();
     });
 
